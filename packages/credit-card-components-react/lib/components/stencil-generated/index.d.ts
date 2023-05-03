fa45700814cc66485831e5ef1a3cb8b63642021c
kukori <attila.varjas@gmail.com>
2023-05-03 11:35:11 +0200

    Fixed ts config and built react version of the components

---
+++ packages/credit-card-components-react/lib/components/stencil-generated/index.d.ts
@@ -0,0 +1,7 @@
+export declare const CardExpirationInput: any;
+export declare const CardFormContainer: any;
+export declare const CardHolderInput: any;
+export declare const CreditCardInput: any;
+export declare const CvvInput: any;
+export declare const ErrorMessage: any;
+export { defineCustomElements } from "credit-card-components-stencil/loader";
---
+++ packages/credit-card-components-react/lib/components/stencil-generated/index.js
@@ -0,0 +1,11 @@
+/* eslint-disable */
+/* tslint:disable */
+/* auto-generated react proxies */
+import { createReactComponent } from './react-component-lib';
+export const CardExpirationInput = /*@__PURE__*/ createReactComponent('card-expiration-input');
+export const CardFormContainer = /*@__PURE__*/ createReactComponent('card-form-container');
+export const CardHolderInput = /*@__PURE__*/ createReactComponent('card-holder-input');
+export const CreditCardInput = /*@__PURE__*/ createReactComponent('credit-card-input');
+export const CvvInput = /*@__PURE__*/ createReactComponent('cvv-input');
+export const ErrorMessage = /*@__PURE__*/ createReactComponent('error-message');
+export { defineCustomElements } from "credit-card-components-stencil/loader";
