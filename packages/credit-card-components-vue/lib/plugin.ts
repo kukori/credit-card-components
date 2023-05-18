ebf8149a99bf53cfa8e2dee5b3727cb1a050a3c8
kukori <attila.varjas@gmail.com>
2023-05-18 16:13:43 +0200

    Redoing vue components

--- packages/credit-card-components-vue/lib/plugin.ts
+++ packages/credit-card-components-vue/lib/plugin.ts
@@ -2,9 +2,9 @@ import { Plugin } from 'vue';
 import { applyPolyfills, defineCustomElements } from 'credit-card-components-stencil/loader';
 
 export const ComponentLibrary: Plugin = {
-    async install() {
-        applyPolyfills().then(() => {
-            defineCustomElements();
-        });
-    },
+  async install() {
+    applyPolyfills().then(() => {
+      defineCustomElements();
+    });
+  },
 };
